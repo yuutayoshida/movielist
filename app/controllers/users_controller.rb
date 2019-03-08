b0cfd1204342138707ee5bed52b2b736d14901c1
yoshida.pickles <ec2-user@ip-172-31-86-84.ec2.internal>
2019-03-08 11:00:28 +0000

    user login

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,5 @@
 class UsersController < ApplicationController
+  before_action :require_user_logged_in, only: [:show]
   def show
     @user = User.find(params[:id])
   end
@@ -17,5 +18,11 @@ class UsersController < ApplicationController
       flash.now[:danger] = 'ユーザの登録に失敗しました。'
       render :new
     end
+  end
+  
+  private
+  
+  def user_params
+    params.require(:user).permit(:name, :email, :password, :password_confirmation)
   end
 end
