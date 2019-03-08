236237efbdec1093d25ed97c9c3f81e686e7c75b
yoshida.pickles <ec2-user@ip-172-31-86-84.ec2.internal>
2019-03-08 08:42:22 +0000

    user registration

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,21 @@
+class UsersController < ApplicationController
+  def show
+    @user = User.find(params[:id])
+  end
+
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)
+    
+    if @user.save
+      flash[:success] = 'ユーザを登録しました。'
+      redirect_to @user
+    else
+      flash.now[:danger] = 'ユーザの登録に失敗しました。'
+      render :new
+    end
+  end
+end
